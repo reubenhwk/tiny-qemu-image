76f35c1a66efd13afdf572737307f6a8b454202a
Reuben Hawkins <reubenhwk@gmail.com>
2019-05-06 15:11:13 -0700

    make a grub boot iso

--- build-iso.sh
+++ build-iso.sh
@@ -2,22 +2,21 @@
 
 mkdir -p cdroot
 mkdir -p cdroot/boot
-mkdir -p cdroot/grub
+mkdir -p cdroot/boot/grub
 
 cp initramfs-busybox-x86.cpio.gz cdroot/boot
 cp bzImage cdroot/boot
-cat <<-EOF > cdroot/grub/grub.cfg
+cat <<-EOF > cdroot/boot/grub/grub.cfg
 	set default="0"
 	set timeout=20
-	menuentry "Ubuntu CLI" {
+	menuentry "Busybox/Linux" {
 	    linux /boot/bzImage textonly
 	    initrd /boot/initramfs-busybox-x86.cpio.gz
-	}
-	menuentry "Memory Test" {
-	    linux16 /boot/memtest86+.bin
 	}
 	menuentry "Boot from the first hard disk" {
 	    set root=(hd0)
 	    chainloader +1
 	}
 	EOF
+
+grub-mkrescue -o image.iso cdroot
